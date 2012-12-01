dc5de0e6ac45cd9f0aca70150cd11f0f690de91a
gabriel-dehan <dehan.gabriel@gmail.com>
2012-12-01 23:38:25 +0100

    added protection from forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
-#  protect_from_forgery
-  rescue_from ActionController::InvalidAuthenticityToken, :with => :invalid_token
-
-  private
-  def invalid_token exception
-    puts 'Invalid Authenticity Token'
-    puts exception
-  end
+  protect_from_forgery
 end
