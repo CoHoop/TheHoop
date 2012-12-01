4827a88c17f847a829da38cf112151f085883ff9
gabriel-dehan <dehan.gabriel@gmail.com>
2012-12-01 23:33:20 +0100

    invalid token logging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
 #  protect_from_forgery
+  rescue_from ActionController::InvalidAuthenticityToken, :with => :invalid_token
+
+  private
+  def invalid_token exception
+    puts 'Invalid Authenticity Token'
+    puts exception
+  end
 end
