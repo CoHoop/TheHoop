9f132b12a48de22d4f31c772672fa03de2dc2afd
Nicolas Lescop <limplementeur@gmail.com>
2012-12-01 17:00:20 -0800

    Add microhoop join and get

--- app/controllers/api/microhoops_controller.rb
+++ app/controllers/api/microhoops_controller.rb
@@ -1,4 +1,6 @@
 class Api::MicrohoopsController < ApplicationController
+	include ActiveSupport
+
   def create
     response = {}
 
@@ -13,5 +15,50 @@ class Api::MicrohoopsController < ApplicationController
     response[:created] = microhoop.tag!(tags, save: true) ? 1 : 0
 
     render json: response
+  end
+
+  def get
+  	microhoop_id = params['microhoop_id']
+
+  	microhoop = Microhoop.find_by_id(microhoop_id) or (microhoop_not_found(microhoop_id) and return)
+
+	  response = {
+	  	is_meeting: microhoop.is_meeting,
+	  	content: microhoop.content,
+	  	location: microhoop.location,
+	  	date: microhoop.created_at,
+	  	user_name: microhoop.user.name,
+	  	microhoop_id: microhoop.id,
+	  	votes: microhoop.votes,
+	  	answers: microhoop.answers.map do |answer| 
+	  		answer.attributes.except('updated_at', 'user_id', 'created_at')
+	  	end
+	  }
+
+  	render json: response
+  end
+
+  def join
+  	response = {
+  		success: 0
+  	}
+
+  	microhoop_id = params['microhoop_id']
+  	user_id = params['user_id']
+
+  	microhoop = Microhoop.find_by_id(microhoop_id)
+
+  	if microhoop
+  		microhoop.is_meeting = true
+  		microhoop.save!
+  		response[:success] = 1
+  	end
+
+  	render json: response
+  end
+
+  private
+  def microhoop_not_found id
+    render :status => 401, :json => { :success => false, :errors => ["Microhoop does not exists for id #{id}."]}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ TheHoop::Application.routes.draw do
     match '/users/update' => 'users#update'
     match '/users/feed/:fb_uid' => 'users#feed'
     match '/microhoop/create' => 'microhoops#create'
+    match '/microhoop/get/:microhoop_id' => 'microhoops#get'
+    match '/microhoop/join' => 'microhoops#join'
     match '/answers/create' => 'answers#create'
     match '/answers/voteup/:answer_id' => 'answers#voteup'
   end
